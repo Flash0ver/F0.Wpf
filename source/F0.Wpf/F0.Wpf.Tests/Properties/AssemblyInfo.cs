0908ab184ae6144b1ee2b5a9a27738e0214e2223
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2018-07-02 23:59:19 +0200

    Created production and test projects
    * Production code project: .NET Framework
    * Test code project: .NET Framework (MSTest)

---
+++ source/F0.Wpf/F0.Wpf.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: AssemblyTitle("F0.Wpf.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("F0.Wpf.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f89e67d5-ad98-4b45-94c5-b4c2feae303a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.ClassLevel)]
